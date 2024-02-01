c7f1be45db52749b384d1d52f1cb6e3b44687b1f
Rizq Yazed <rizqyazed@Rizqs-MBP.broadband>
2024-02-01 23:05:41 +0000

    hours

--- js/index.js
+++ js/index.js
@@ -1,17 +1,11 @@
-var d = new Date(); // for now   
-const current = d.getTime();
-// increase the date by 1
-d.setDate(d.getDate() + 1);
-// set hours, mins, seconds to the 0 [as setHours can accepts (hours, mins, seconds, miliseconds), we can pass all the parameters at once]
-d.setHours(0, 0, 0, 0);
-// now currDate holds the time for midnight
-const midnight = d.getTime();
-// create a setTimeout function for the time difference
-const diff = midnight - current;
+var d = new Date(); // for now  
 
+console.log(d.getHours())
 
-console.log(`Code will be triggered after: ${diff}ms`);
-setInterval(setGradient, diff);   
+if (d.getHours() === 0) {
+    console.log("CHANGE")
+    setGradient();
+}
 
 function setGradient(){   
     localStorage.setItem("c1", randomColor());
